72f8cc18f678d51989165efe301db0b77fa2c3eb
Abdullah Adel <abdullahadel365@gmail.com>
2025-11-12 04:34:07 +0200

    m

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Layout from './Layout/Layout';
+import Error from './pages/Error/Error';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout /> ,
-        // errorElement: <Error />,
+        errorElement: <Error />,
         children: [
         { path: '/', element: <Home /> },
-
         ],
     },
 
